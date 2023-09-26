ed817d26650da370ce7bfb2b208afe79a11801fa
Jordan <jordandearaujo@outlook.com>
2023-09-25 22:01:28 -0300

    Project started

---
+++ src/main/java/com/holding/springbootapp/Controller.java
@@ -0,0 +1,13 @@
+package com.holding.springbootapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/home")
+    public String Welcome(){
+        return "Hello World!";
+    }
+}
